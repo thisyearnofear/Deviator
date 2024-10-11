1aec6885a048bbabe0e912e7e1590e7da4db33c5
Quincybob <74205675+Quincybob@users.noreply.github.com>
2024-10-11 19:16:23 +0100

    npm init

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,30 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: "./game.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
-  resolve: {
-    extensions: [".js"], // Automatically resolve these extensions
-    alias: {
-      components: path.resolve(__dirname, "components/"), // Alias for components folder
-      utils: path.resolve(__dirname, "utils/"), // Alias for utils folder
-    },
-  },
   devServer: {
-    static: "./", // Serve the root directory (where index.html is located)
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
+    compress: true,
     port: 8080,
   },
 };
